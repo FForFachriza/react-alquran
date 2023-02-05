f4921be5a4874366ba94c1ba6316bac601356f69
fforfachriza <dimasavrian346@gmail.com>
2023-02-05 18:34:41 +0700

    sitfos

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,7 +4,9 @@ import Navbar from '../Components/Navbar'
 const About = () => {
   return (
     <>
-    <div className='p-12'>Kapan Kapan ajlh kalo gk mager</div>
+    <div className='p-12'>Kapan Kapan ajlh kalo gk &nbsp;
+       <a className='link' href="https://github.com/fforfachriza">mager</a>
+       </div>
     <Navbar />
     </>
   )
